6b2cd5aa755b9114dabc0c8f19fcc4127d019879
Giannah <claudine.lafine@gmail.com>
2018-12-28 17:51:54 +0100

    Updates

---
+++ precache-manifest.0edaf8be4d2d1c3c283e9f9e56d33807.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "07ee2f056e73d4ff7c5b3ae165a6e3bc",
+    "url": "/guestlist/static/media/header.07ee2f05.jpg"
+  },
+  {
+    "revision": "4145280f529c8088de62",
+    "url": "/guestlist/static/js/runtime~main.4145280f.js"
+  },
+  {
+    "revision": "907bc87fa49ea2ab6dd2",
+    "url": "/guestlist/static/js/main.907bc87f.chunk.js"
+  },
+  {
+    "revision": "f4789f78bc5099d1ff6c",
+    "url": "/guestlist/static/js/1.f4789f78.chunk.js"
+  },
+  {
+    "revision": "907bc87fa49ea2ab6dd2",
+    "url": "/guestlist/static/css/main.6275f2e8.chunk.css"
+  },
+  {
+    "revision": "136dc369bc77a24292de032f668f0017",
+    "url": "/guestlist/index.html"
+  }
+];
